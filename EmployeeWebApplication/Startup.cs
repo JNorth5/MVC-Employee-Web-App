9c624572173573659a5d462f486e76fe9061c002
Jacob Q. North <jnorth5@hotmail.com>
2019-11-01 16:38:01 -0500

    Add project files.

---
+++ EmployeeWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeeWebApplication.Startup))]
+namespace EmployeeWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
